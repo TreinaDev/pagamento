6a14d7d7c2c90435d3b40bd2a8112233d7daece5
Andre e Mirian <mirian.iosimura@gmail.com>
2020-02-03 22:03:53 -0300

    WIP client sees payments methods

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :client do
-    
+    email { 'marcelle.sa@teste.com' }
+    password { '12345678' }
   end
 end
---
+++ spec/features/client_views_payment_methods_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+feature 'client views payments methods' do
+  scenario 'sucessfully' do
+    
+  end
+end
