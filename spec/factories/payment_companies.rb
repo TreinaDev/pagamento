203237b98e00fb2f85a1154a7b924e27db8809b9
mariana castro <mariananycastro@gmail.com>
2020-02-10 21:55:45 -0300

    Add authentication to payment company
    Co-authored-by: Rubens <igesca.rubens@gmail.com>

--- spec/factories/payment_companies.rb
+++ spec/factories/payment_companies.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     name { 'MasterCard' }
     image_path = Rails.root.join('spec/support/mastercard.png')
     image { fixture_file_upload(image_path, 'image/png') }
+    payment_method
   end
 end
