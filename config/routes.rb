42fd452bf5fe501470bad62d6bff8eae50c26097
Larissa Porto <larissa.silva86@gmail.com>
2020-02-11 21:08:07 -0300

    WIP api v1 routes
    Co-authored-by: Rubens <igesca.rubens@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,12 @@ Rails.application.routes.draw do
     resources :dashboard, only: %i[index]
   end
 
+  namespace :api do
+    namespace :v1 do
+      resources :payment_settings, only: %i[index]
+    end
+  end
+
   resources :payment_methods, only: %i[index show new create]
   resources :payment_settings, only: %i[show new create]
 
