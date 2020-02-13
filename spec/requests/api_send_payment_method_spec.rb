7809eb2a9c0bafa704b907147c098e0585329ed5
mariana castro <mariananycastro@gmail.com>
2020-02-12 22:18:14 -0300

    WIP image blob to companies
    Co-authored-by: Gustavo Carvalho <gustavo.hcarvalho@outlook.com>

--- spec/requests/api_send_payment_method_spec.rb
+++ spec/requests/api_send_payment_method_spec.rb
@@ -13,6 +13,66 @@ describe 'API sends payment methods' do
     json = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to have_http_status(:ok)
-    expect(json[:data]).to include('Cartão de Debito')
+    expect(json[:data][0][:name]).to include('Cartão de Debito')
+  end
+
+  it 'successfully and render correct json' do
+    client1 = create(:client)
+    client2 = create(:client, email: 'teste@teste.com')
+    client_profile1 = create(:client_profile, auth_token: 'ABC1234567', client: client1)
+    client_profile2 = create(:client_profile, auth_token: 'ABCDE12345', client: client2)
+    payment_method1 = create(:payment_method, :debit)
+    payment_method2 = create(:payment_method, :boleto)
+    create(:payment_setting, client_profile: client_profile1,
+                               payment_method: payment_method1)
+    create(:payment_setting, client_profile: client_profile2,
+                                payment_method: payment_method2)
+
+    get api_v1_payment_methods_path(auth_token: client_profile1.auth_token)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to have_http_status(:ok)
+    expect(json[:data][0][:name]).to include('Débito em Conta')
+    expect(json[:data]).not_to include('Boleto')
+  end
+
+  it 'successfully and render correct json' do
+    client1 = create(:client)
+    client_profile1 = create(:client_profile, auth_token: 'ABC1234567', client: client1)
+    payment_method1 = create(:payment_method, :debit)
+    payment_method2 = create(:payment_method, :boleto)
+    payment_settings1 = create(:payment_setting, client_profile: client_profile1,
+                               payment_method: payment_method1)
+    payment_settings2 = create(:payment_setting, client_profile: client_profile1,
+                                payment_method: payment_method2)
+
+    get api_v1_payment_methods_path(auth_token: client_profile1.auth_token)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+     
+    expect(response).to have_http_status(:ok)
+    expect(json[:data][0][:name]).to include('Débito em Conta')
+    expect(json[:data][1][:name]).to include('Boleto')
+  end
+
+  it 'successfully render payment companies' do
+    image_path = Rails.root.join('spec/support/mastercard.png')
+    client = create(:client)
+    client_profile = create(:client_profile, auth_token: 'ABC1234567', client: client)
+    payment_method = create(:payment_method, name: 'Cartão de Débito')
+    company = create(:payment_company, payment_method: payment_method)
+    create(:payment_setting, client_profile: client_profile,
+                              payment_method: payment_method)
+
+    get api_v1_payment_methods_path(auth_token: client_profile.auth_token)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    
+    pp json
+
+    expect(response).to have_http_status(:ok)
+    expect(json[:data][0][:name]).to include('Cartão de Débito')
+    expect(json[:data][0][:payment_company][:image]).to include(company.image)
   end
 end
