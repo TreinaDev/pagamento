7140e70d15c1600384ee83f5b0863ea1c0c36c4f
Rubens Igesca <igesca.rubens@gmail.com>
2020-02-14 22:03:16 -0300

    Improved client_profile, change the created method, implement validation and teste

--- app/models/client_profile.rb
+++ app/models/client_profile.rb
@@ -1,7 +1,12 @@
 class ClientProfile < ApplicationRecord
   belongs_to :client
-  has_secure_token :auth_token
+
   has_many :payment_settings, dependent: :destroy
   has_many :payment_methods, through: :payment_settings
+
+  has_secure_token :auth_token
   accepts_nested_attributes_for :client
+  validates_associated :client
+
+  validates :cnpj, :company_name, presence: true
 end
--- spec/features/admin/admin_register_new_client_spec.rb
+++ spec/features/admin/admin_register_new_client_spec.rb
@@ -12,7 +12,7 @@ feature 'Admin register a new client' do
     fill_in 'Representante', with: 'Luizinho'
     fill_in 'Endereço', with: 'Av. Teste, 1000'
     fill_in 'Telefone', with: '(11)2222-9999'
-    fill_in 'Email', with: 'client@teste.com'
+    fill_in 'E-mail', with: 'client@teste.com'
 
     click_on 'Criar'
 
@@ -27,5 +27,28 @@ feature 'Admin register a new client' do
 
     expect(client_profile.auth_token).not_to eq(nil)
     expect(page).to have_content(client_profile.auth_token)
+  end
+
+  it 'and try to create profile withou a email' do
+    admin = create(:admin)
+    login_as(admin, scope: :admin)
+
+    visit new_admin_client_profile_path
+
+    click_on 'Criar'
+
+    expect(page).to have_content('E-mail não pode ficar em branco')
+  end
+
+  it 'and try to create profile witout cnpj and company name' do
+    admin = create(:admin)
+    login_as(admin, scope: :admin)
+
+    visit new_admin_client_profile_path
+    fill_in 'E-mail', with: 'client@teste.com'
+    click_on 'Criar'
+
+    expect(page).to have_content('CNPJ não pode ficar em branco')
+    expect(page).to have_content('Razão Social não pode ficar em branco')
   end
 end
