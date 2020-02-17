51eb681acc58a0f154157f177798ac5179df2e34
Bruno e Kelly <brunorsilva_@hotmail.com>
2020-02-17 20:36:14 -0300

    API: Receive order and confirm payment
    Co-authored-by: kelly <kellycruz.ti@gmail.com>

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,27 +1,30 @@
 module Api
   module V1
     class OrdersController < ApiController
+      rescue_from ActiveRecord::RecordNotFound, with: :client_invalid
+      rescue_from ActiveRecord::RecordInvalid, with: :order_invalid
+
       def create
-        client = ClientProfile.find_by(auth_token: params[:client_token])
-        if client.nil?
-          render json: { "message": 'Cliente não registrado' },
-                 status: :precondition_failed
-          return
-        end
+        client = ClientProfile.find_by!(auth_token: params[:client_token])
         order = client.orders.new(order_params)
-        if order.valid?
-          order.save!
-          render json: { order: order, client: order.client,
-                         payment_method: order.payment_method },
-                 status: :created
-        else
-          render json: { "message": order.errors.full_messages },
-                 status: :precondition_failed
-        end
+        order.save!
+        render json: { order: order, client: order.client_profile,
+                       payment_method: order.payment_method },
+               status: :created
       end
 
       private
-      
+
+      def client_invalid
+        render json: { "message": 'Cliente não registrado' },
+               status: :precondition_failed
+      end
+
+      def order_invalid(exception)
+        render json: { "message": exception.record.errors.full_messages },
+               status: :precondition_failed
+      end
+
       def order_params
         params.permit(:client_token, :order_id, :order_value,
                       :payment_method_id)
