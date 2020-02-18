5fa5143a0a79f395b2cd06142fd50267d57b67b9
Gustavo Carvalho <gustavo.hcarvalho@outlook.com>
2020-02-18 14:43:28 -0300

    Merged boostrap branch and correct views
    Co-authored-by: Mariana Castro <mariananycastro@gmail.com>

--- spec/features/admin/admin_edits_client_profile_spec.rb
+++ spec/features/admin/admin_edits_client_profile_spec.rb
@@ -2,52 +2,57 @@ require 'rails_helper'
 
 feature 'admin edits client profile' do
   scenario 'successfully' do
-    client1 = Client.create!(email: 'client1@test.com', password: '123456')
-    client_profile1 = ClientProfile.create!(
-      company_name: 'A', cnpj: '111111111', manager: 'Felipe',
-      address: 'Rua A, 1234', phone: '11-2222222',
-      auth_token: '123456', client: client1
-    )
+    client = Client.create!(email: 'client1@test.com', password: '123456')
+    client_profile = ClientProfile.create!(company_name: 'A',
+                                           cnpj: '111111111',
+                                           manager: 'Felipe',
+                                           address: 'Rua A, 1234',
+                                           phone: '11-2222222',
+                                           auth_token: '123456',
+                                           client: client)
 
     admin = create(:admin)
     login_as(admin, scope: :admin)
 
     visit admin_client_profiles_path
-    click_on client_profile1.company_name
+    click_on client_profile.company_name
+    click_on 'Editar'
     fill_in 'CNPJ', with: '78.991.081/0001-26'
-    fill_in 'Razão Social', with: 'Campos Code'
+    fill_in 'Razão Social', with: 'Campus Code'
     fill_in 'Representante', with: 'Luizinho'
     fill_in 'Endereço', with: 'Av. Teste, 1000'
     fill_in 'Telefone', with: '(11)2222-9999'
     click_on 'Salvar'
 
+    client_profile = ClientProfile.last
+
     expect(page).to have_content('Perfil do Cliente')
     expect(page).to have_content('78.991.081/0001-26')
-    expect(page).to have_content('Campos Code')
+    expect(page).to have_content('Campus Code')
     expect(page).to have_content('Luizinho')
     expect(page).to have_content('Av. Teste, 1000')
     expect(page).to have_content('(11)2222-9999')
     expect(page).to have_content('Perfil do cliente atualizado com sucesso')
-
-    client_profile = ClientProfile.last
-
     expect(client_profile.auth_token).not_to eq(nil)
     expect(client_profile.auth_token).to eq('123456')
   end
 
-  scenario 'fields cant be blanck' do
-    client1 = Client.create!(email: 'client1@test.com', password: '123456')
-    client_profile1 = ClientProfile.create!(
-      company_name: 'A', cnpj: '111111111', manager: 'Felipe',
-      address: 'Rua A, 1234', phone: '11-2222222',
-      auth_token: '123456', client: client1
-    )
+  scenario 'fields cant be blank' do
+    client = Client.create(email: 'client1@test.com', password: '123456')
+    client_profile = ClientProfile.create(company_name: 'A',
+                                          cnpj: '111111111',
+                                          manager: 'Felipe',
+                                          address: 'Rua A, 1234',
+                                          phone: '11-2222222',
+                                          auth_token: '123456',
+                                          client: client)
 
     admin = create(:admin)
     login_as(admin, scope: :admin)
 
     visit admin_client_profiles_path
-    click_on client_profile1.company_name
+    click_on client_profile.company_name
+    click_on 'Editar'
     fill_in 'CNPJ', with: ''
     fill_in 'Razão Social', with: ''
     fill_in 'Representante', with: ''
